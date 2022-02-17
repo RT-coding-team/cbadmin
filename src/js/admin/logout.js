c66aa9643a6f1783c481d04f2ec3c8aa5696aa6a
Derek Maxson <dmaxson@frontporch.com>
2022-02-17 15:54:12 -0800

    Logout via API

--- src/js/admin/logout.js
+++ src/js/admin/logout.js
@@ -1,3 +1,4 @@
+import {API_URL, get} from "../api/api";
 /**
  * Forget token and send back user to login page
  * @param setToken
@@ -5,5 +6,6 @@
 export default function logout(setToken) {
     setToken(null);
     localStorage.removeItem('admin-authorization');
+    get(`${API_URL}logout`);
     window.location = "/admin/login.html";
 }
